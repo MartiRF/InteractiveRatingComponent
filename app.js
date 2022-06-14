c7385e0ab63efcb99951d6807c78047e047598c8
Martin Reyes <martinreyesrf@gmail.com>
2022-06-14 14:40:59 -0500

    Falta que muestre el ranking en la card de gracias

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-const valorBoton = document.querySelector('input')
-console.log(valorBoton)
+
+const crearCardGracias = (valor)=>{
+    let cardHTML = `<section class="card_gracias">
+    <img src="images/illustration-thank-you.svg" alt="Gracias">
+    <p class="etiqueta">You selected ${valor} out of 5</p>
+    <h1>Thank you!</h1>
+    <p>We appreciate you taking the time to give a rating. If you ever need more support, 
+      don’t hesitate to get in touch!</p>
+  </section> -->`
+  return cardHTML;
+}
+const cargarGracias = (valorBTN)=>{
+    document.getElementById('body').innerHTML = crearCardGracias();
+}
